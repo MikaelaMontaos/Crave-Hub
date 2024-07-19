68418162555fad9fb58c6df68cb7dd5c649e3c84
Mikaela M. <59987624+MikaelaMontaos@users.noreply.github.com>
2024-07-18 20:51:39 -0700

    Update Crave_Hub_script.js
    07/18 collab

---
+++ Crave_Hub_script.js
@@ -0,0 +1,13 @@
+function AccountPage() {
+  
+}
+
+function OrdersPage() {
+  
+}
+
+function SearchZip() {
+  event.preventDefault();
+  
+  document.getElementById("restaurant_list").style.display = "block";
+}
