c02923cbddfa5102cb666219212f1a587923bfee
Mikaela M. <mvamontaos@ymail.com>
2024-07-22 10:31:20 -0700

    Update Crave_Hub_script.js

--- Crave_Hub_script.js
+++ Crave_Hub_script.js
@@ -1,13 +1,26 @@
 function AccountPage() {
-  
+  event.preventDefault();
+  document.getElementById("account").style.display = "block";
+
+  // Hide other divs
+  document.getElementById("orders").style.display = "none";
+  document.getElementById("restaurant_list").style.display = "none";
 }
 
 function OrdersPage() {
-  
+  event.preventDefault();
+  document.getElementById("orders").style.display = "block";
+
+  // Hide other divs
+  document.getElementById("account").style.display = "none";
+  document.getElementById("restaurant_list").style.display = "none";
 }
 
 function SearchZip() {
   event.preventDefault();
-  
   document.getElementById("restaurant_list").style.display = "block";
+
+  // Hide other divs
+  document.getElementById("account").style.display = "none";
+  document.getElementById("orders").style.display = "none";
 }
