b65bac4b14b39cf855d12050de6bc71397a044e3
Mikaela M. <mvamontaos@ymail.com>
2024-07-24 15:20:15 -0700

    javascript and update on html

--- Crave_Hub_script.js
+++ Crave_Hub_script.js
@@ -1,3 +1,4 @@
+// Menu bar functions
 function AccountPage() {
   event.preventDefault();
   document.getElementById("account").style.display = "block";
@@ -6,14 +7,6 @@ function AccountPage() {
   document.getElementById("orders").style.display = "none";
   document.getElementById("restaurant_list").style.display = "none";
 }
-
-function Liberty() {
-  event.preventDefault();
-  document.getElementById("liberty_menu").style.display = "block";
-
-  // Hide other divs
-
-} 
 
 function OrdersPage() {
   event.preventDefault();
@@ -24,16 +17,44 @@ function OrdersPage() {
   document.getElementById("restaurant_list").style.display = "none";
 }
 
+
+// Search functions
 function SearchZip() {
   event.preventDefault();
-  if(document.getElementById("search_zip_menu").value != "" || document.getElementById("ZIP").value != "") {
+  if (document.getElementById("search_zip_menu").value != "" || document.getElementById("ZIP").value != "") {
     document.getElementById("restaurant_list").style.display = "block";
+    document.getElementById("search_cuisine").style.display = "block";
 
     // Hide other divs
-  document.getElementById("account").style.display = "none";
-  document.getElementById("orders").style.display = "none";
+    document.getElementById("account").style.display = "none";
+    document.getElementById("orders").style.display = "none";
   }
   else {
     alert("Missing zip");
   }
+}
+
+function SearchAmerican() {
+  event.preventDefault();
+  document.getElementById("american").style.display = "block";
+}
+
+function SearchItalian() {
+  event.preventDefault();
+  document.getElementById("italian").style.display = "block";
+}
+
+function SearchJapanese() {
+  event.preventDefault();
+  document.getElementById("japanese").style.display = "block";
+}
+
+
+// Restaurant functions
+function Liberty() {
+  event.preventDefault();
+  document.getElementById("liberty_menu").style.display = "block";
+
+  // Hide other divs
+  document.getElementById("search_cuisine").style.display = "none";
 }
